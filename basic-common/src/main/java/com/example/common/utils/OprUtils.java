54d21912c6982765d5e4983c4463ed19a87aaf53
vegetableOnlyBecaus <382005688@qq.com>
2023-02-18 16:22:32 +0800

    工具类优化

--- basic-common/src/main/java/com/example/common/utils/OprUtils.java
+++ basic-common/src/main/java/com/example/common/utils/OprUtils.java
@@ -3,11 +3,7 @@ package com.example.common.utils;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.BeanUtils;
 
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -20,19 +16,15 @@ import java.util.function.Function;
 public class OprUtils {
 
     public static <T, U> U copyModel2Model(T model, U result){
-        if (null == model) {
-            return null;
-        }
+        Objects.requireNonNull(model);
         BeanUtils.copyProperties(model, result);
         return result;
     }
 
     public static <T, U> U model2Model(T model, U value, Consumer<U> consumer){
-        U result = copyModel2Model(model, value);
-        if (null != result && null != consumer) {
-            consumer.accept(result);
-        }
-        return result;
+        copyModel2Model(model, value);
+        Optional.ofNullable(consumer).ifPresent(x -> x.accept(value));
+        return value;
     }
 
     public static <T, U> List<U> models2Models(List<T> models, Function<T, U> function) {
@@ -40,22 +32,20 @@ public class OprUtils {
             return Collections.emptyList();
         }
         List<U> result = new ArrayList<>();
-        for (T model : models) {
+        models.forEach(model -> {
             U value = function.apply(model);
-            if (null != value) {
-                result.add(value);
-            }
-        }
+            Optional.ofNullable(value).ifPresent(result::add);
+        });
         return result;
     }
 
     public static <T> void checkAndDeal(T model, Consumer<T> consumer) throws Exception {
-        T oprModel = checkModel(model);
-        consumer.accept(oprModel);
+        checkModel(model);
+        consumer.accept(model);
     }
 
-    public static <T> T checkModel(T model) throws Exception {
-        return Optional.ofNullable(model).orElseThrow(() -> new Exception("信息不存在:" + model.getClass()));
+    public static <T> void checkModel(T model) throws Exception {
+        Objects.requireNonNull(model, "信息不存在:" + model.getClass());
     }
 
 }
